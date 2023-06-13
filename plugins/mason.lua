7068572f38f336924d080403e6d6181aad458a44
MadokaIII <daoudlamalmi@gmail.com>
2023-06-13 20:40:16 +0200

    Automated commit: Changes detected

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -25,16 +25,7 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "clang-format",
-        "stylua",
-        "omnisharp",
-        "rustfmt",
-        "black",
-        "gofumpt",
-        "yamlfmt",
-        "jsonlint",
-      })
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {})
     end,
   },
   {
@@ -42,13 +33,7 @@ return {
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "cpptools",
-        "netcoredbg",
-        "solargraph",
-        "debugpy",
-        "delve",
-      })
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {})
     end,
   },
 }
