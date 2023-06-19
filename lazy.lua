08db27bf7d74e65ec244197d09c65750dedc6739
MadokaIII <daoudlamalmi@gmail.com>
2023-06-20 00:04:25 +0200

    Automated commit: Changes detected

---
+++ lazy.lua
@@ -0,0 +1,19 @@
+return {
+  -- Configure require("lazy").setup() options
+  lazy = {
+    defaults = { lazy = true },
+    performance = {
+      rtp = {
+        -- customize default disabled vim plugins
+        disabled_plugins = {
+          "tohtml",
+          "gzip",
+          "matchit",
+          "zipPlugin",
+          "netrwPlugin",
+          "tarPlugin",
+        },
+      },
+    },
+  },
+}
